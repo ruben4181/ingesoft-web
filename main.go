3197852e60d3ec5eccb99603f307c7cd593d8a7e
ruben4181 <ruben4181@gmail.com>
2018-10-16 22:43:19 -0500

    Ya todo esta listo para comenzar con la parte Web... Ya esta hecho lo que se necesita para el primer avance en la parte del backend

--- main.go
+++ main.go
@@ -5,9 +5,12 @@ import(
 	_ "github.com/go-sql-driver/mysql"
 	"database/sql"
 	"log"
+	"strconv"
 )
 
 var database *sql.DB;
+
+//Definicion de las estructuras de datos
 
 type User struct{
 	ID_user int;
@@ -16,6 +19,67 @@ type User struct{
 	Username string;
 	Email string;
 	Password string;
+}
+
+type Program struct{
+	ID_program int;
+	Program_name string;
+	Program_description string;
+	ID_user int;
+}
+type Post struct{
+	ID_post int;
+	Post_title string;
+	Post_abstract string;
+	Post_body string;
+	ID_user int;
+	ID_program int;
+}
+
+type InternalResponse struct{
+	Status int;
+	Description string;
+}
+
+//Funciones que interactuan con la base de datos
+
+const OK_STATUS=0;
+const USER_PASS_NOT_MATCH=1;
+const ERR_STATUS=-1;
+const EXCEPTION_STATUS=-2;
+
+func GetPosts(condition string) []Post{
+	var posts []Post;
+	data, err:=database.Query("SELECT id_post, post_title, post_abstract, post_body, id_user, id_program FROM posts WHERE "+condition);
+	if err!=nil{
+		fmt.Println("An error ocurred during executing query in GetPosts function");
+		log.Fatal(err);
+	}else{
+		for data.Next(){
+			var id_post, id_user, id_program int;
+			var post_title, post_abstract, post_body string;
+			err2:=data.Scan(&id_post, &post_title, &post_abstract, &post_body, &id_user, &id_program);
+			if err2!=nil{
+				fmt.Println("Error while scanning result from query in post");
+				log.Fatal(err);
+			}else{
+				posts=append(posts, Post{ID_post:id_post, Post_title:post_title, Post_abstract:post_abstract, 
+					Post_body:post_body, ID_user:id_user, ID_program:id_program});
+			}
+		}
+	}
+	return posts;
+}
+
+func NewPost(post Post){
+	_, err:=database.Query("INSERT INTO posts(post_title, post_abstract, post_body, id_user, id_program) VALUES('"+
+		post.Post_title+"', '"+post.Post_abstract+"', '"+post.Post_body+"', "+strconv.Itoa(post.ID_user)+", "+strconv.Itoa(post.ID_program)+")");
+	if err!=nil{
+		fmt.Println("An error ocurred while insert in posts' table");
+		log.Fatal(err);
+	}else{
+		fmt.Println("New Post has been added");
+	}
 }
 
 func OpenDB(user string, password string) *sql.DB{
@@ -29,40 +93,82 @@ func OpenDB(user string, password string) *sql.DB{
 	return db;
 }
 
-func GetUser(values string, condition string) User{
+func GetUser(condition string) User{
 	var user User;
 	
-	if (values==""){
-		data, err:=database.Query("SELECT id_user, firstname, lastname, username, email, password FROM users WHERE "+condition);
-		if err!=nil{
-			fmt.Println("An error ocurred during the query in GetUser function");
-			log.Fatal(err);
-		}else{
-			var id_user int;
-			var firstname, lastname, username, email, password string;
-			for data.Next(){
-				err2:=data.Scan(&id_user, &firstname, &lastname, &username, &email, &password);
-				if(err2!=nil){
-					fmt.Println("Error while scanning result from query in GetUser function");
-					log.Fatal(err2);
-				}else{
-					user.ID_user=id_user;
-					user.Firstname=firstname;
-					user.Lastname=lastname;
-					user.Username=username;
-					user.Email=email;
-					user.Password=password;
-				}
+	data, err:=database.Query("SELECT id_user, firstname, lastname, username, email, password FROM users WHERE "+condition);
+	if err!=nil{
+		fmt.Println("An error ocurred during executing query in GetUser function");
+		log.Fatal(err);
+	}else{
+		var id_user int;
+		var firstname, lastname, username, email, password string;
+		for data.Next(){
+			err2:=data.Scan(&id_user, &firstname, &lastname, &username, &email, &password);
+			if(err2!=nil){
+				fmt.Println("Error while scanning result from query in GetUser function");
+				log.Fatal(err2);
+			}else{
+				user.ID_user=id_user;
+				user.Firstname=firstname;
+				user.Lastname=lastname;
+				user.Username=username;
+				user.Email=email;
+				user.Password=password;
 			}
 		}
-	}else{
-
 	}
 	return user;
 }
 
+func AuthenticateUser(username string, password string, action string) InternalResponse{
+	user:=GetUser("username='"+username+"' AND password='"+password+"'");
+	if user.Username!=""{
+		if user.Username==username{
+			return InternalResponse{Status : OK_STATUS, Description:"Well done!"};
+		}else{
+			return InternalResponse{Status : EXCEPTION_STATUS, Description:"Something went wrong"};
+		}
+	}
+	return InternalResponse{Status : USER_PASS_NOT_MATCH, Description:"Username and/or Password Don't match"}
+}
+
+func GetPrograms(condition string) []Program{
+	var programs []Program;
+
+	data, err:=database.Query("SELECT id_program, program_name, Program_description, id_user FROM programs WHERE "+condition);
+	if err!=nil{
+		fmt.Println("An error ocurred during query in GetPrograms function");
+		log.Fatal(err);
+	}else{
+		for data.Next(){
+			var id_program, id_user int;
+			var program_name string;
+			var program_description string;
+			err2:=data.Scan(&id_program, &program_name, &program_description, &id_user);
+			if err2!=nil{
+				fmt.Println("Error while scanning result from query in GetPrograms function");
+				log.Fatal(err2);
+			}else{
+				programs=append(programs, Program{ID_program:id_program, Program_name:program_name, Program_description:program_description, ID_user:id_user});
+			}
+		}
+	}
+	return programs;
+}
+
+//Funciones que interactuan con el Frontend
+
+//Main
+
 func main() {
 	database=OpenDB("root", "Californication16");
-	user:=GetUser("", "username='ruben4181'");
+	user:=GetUser("username='ruben4181'");
 	fmt.Println(user);
+	programs:=GetPrograms("1");
+	fmt.Println(programs);
+	posts:=GetPosts("id_program=1");
+	fmt.Println(posts);
+	NewPost(Post{ID_post:0, Post_title:"Titulo Post 4", Post_abstract:"-Abstract Post 4", Post_body:"Body Post en algun formato", ID_user:3, ID_program:1});
+	fmt.Println(AuthenticateUser("ruben4181", "Dadada", "None"));
 }
